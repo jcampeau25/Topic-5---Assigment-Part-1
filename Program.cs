92408decbb36593a2c5b498b7d1462c6c4f91312
jcampeau25 <jdccanuck25@gmail.com>
2024-09-23 09:30:42 -0400

    finished

--- Program.cs
+++ Program.cs
@@ -6,26 +6,45 @@
         {
             double weight, planetWeight;
             int planet;
+            planetWeight = 0;
+            Boolean valid = true;
             Console.WriteLine("Please enter your current Earth weight (lbs):");
             double.TryParse(Console.ReadLine(), out weight);
             Console.WriteLine("I have information on the following planets:");
             Console.WriteLine(" 1. Venus  2. Mars   3. Jupiter");
             Console.WriteLine(" 4. Saturn 5. Uranus 6. Neptune");
             Console.WriteLine("Please enter the number of the planet you would like to visit");
+            Console.WriteLine();
             Int32.TryParse(Console.ReadLine(), out planet);
-
-            if (planet == 1);
+            
+            
+            if (planet == 1)
                 planetWeight = weight * 0.78;
-            if (planet == 2);
+            else if (planet == 2)
                 planetWeight = weight * 0.39;
-            if (planetWeight == 3)
+            else if (planet == 3)
+                planetWeight = weight * 2.65;
+            else if (planet == 4)
+                planetWeight = weight * 1.17;
+            else if (planet == 5)
+                planetWeight = weight * 1.05;
+            else if (planet == 6)
+                planetWeight = weight * 1.23;
+            else
+            {
+                valid = false;
+                Console.WriteLine();
+                Console.WriteLine("Invalid Input");
+            }
 
 
 
 
-
-            Console.WriteLine("Your weight on that planet would be " + planetWeight + " lbs");
-
+            if (valid == true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Your weight on that planet would be " + planetWeight + " lbs!");
+            }
         }
     }
 }
